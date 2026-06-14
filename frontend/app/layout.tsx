1547c691b66ad33d7930193e88c637460a4ca430
Felix Muia <felixmut2024@gmail.com>
2026-06-14 20:17:49 +0300

    Fixed the Inter font issue

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
+import '@fontsource/inter';
 import '@/styles/globals.css';
 import RootLayoutWrapper from './layout-wrapper';
 
